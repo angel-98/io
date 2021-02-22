f24d412d126c1015979b9b5503865d8068a3cb21
Angel Gonzalez <angel.gonzalez.osorio9@gmail.com>
2021-02-22 16:41:46 -0600

    nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AgDispComponent} from './dispositivos/ag-disp/ag-disp.component';
+import {AgDetalleComponent} from './dispositivos/ag-detalle/ag-detalle.component';
+import {AgIotComponent} from './dispositivos/ag-iot/ag-iot.component';
+const routes: Routes = [
+	{path:'', component: AgIotComponent},
+	{path:'create', component:AgDispComponent}
+	
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NuevoCComponent } from './nuevo-c/nuevo-c.component';
+import { AgDispComponent } from './dispositivos/ag-disp/ag-disp.component';
+import { AgDetalleComponent } from './dispositivos/ag-detalle/ag-detalle.component';
+import { FormsModule } from '@angular/forms';
+import { DispServices } from './dispositivos/disp.service';
+//-------------------
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+//---------
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+//http
+import { HttpClientModule } from '@angular/common/http';
+import { AgIotComponent } from './dispositivos/ag-iot/ag-iot.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoCComponent,
+    AgDispComponent,
+    AgDetalleComponent,
+    AgIotComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatPaginatorModule
+
   ],
-  providers: [],
+  providers: [DispServices],
   bootstrap: [AppComponent]
 })
+// <mat-slider min="1" max="100" step="1" value="1"></mat-slider>
 export class AppModule { }
